d8e5229ae6882b42507471df4b347039ccd1e4ba
Kamil Kiewisz <kamkie@outlook.com>
2019-10-20 17:44:53 +0200

    permissions in openshift

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 if [ $# -eq 0 ]; then
   echo "No arguments supplied - tail -f /var/log/podman.log"
-  podman info >/var/log/podman.log
+  podman info > /tmp/podman.log
 
-  tail -f /var/log/podman.log
+  tail -f /tmp/podman.log
 fi
 
 echo "executing passed command"
