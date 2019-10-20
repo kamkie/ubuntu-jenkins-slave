77d824c20561e3b7bcc61461b0616aaea477c7e1
Kamil Kiewisz <kamkie@outlook.com>
2019-10-20 20:39:19 +0200

    jenkins remoting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ if [ $# -eq 0 ]; then
 
   sudo -b dockerd --iptables=false -H unix:///var/run/docker.sock -H tcp://127.0.0.1:2375
 
-  mkdir -p "${JENKINS_AGENT_WORKDIR}"
-  cd "${JENKINS_AGENT_WORKDIR}"
-  curl "${JENKINS_URL}jnlpJars/agent.jar" -o agent.jar
-  java -jar agent.jar -jnlpUrl "${JENKINS_URL}computer/${JENKINS_AGENT_NAME}/slave-agent.jnlp" -secret "${JENKINS_SECRET}" -workDir "${JENKINS_HOME}"
+  mkdir -p "${JENKINS_HOME}"
+  cd "${JENKINS_HOME}"
+  curl "${JENKINS_URL}jnlpJars/remoting.jar" -o remoting.jar
+  java -cp remoting.jar hudson.remoting.jnlp.Main -headless -url "${JENKINS_URL}" -tunnel "${JENKINS_TUNNEL}" "${JENKINS_SECRET}" ${JENKINS_AGENT_NAME}
 fi
 
 echo "executing passed command"
